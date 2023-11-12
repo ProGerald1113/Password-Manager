94920d135941a2f6b4d59c69a1090e9fa8e99807
ProGerald1113 <brown.gerald.2008@gmail.com>
2023-11-12 22:06:18 +0000

    Password Strength Checker

--- Strength_meter.py
+++ Strength_meter.py
@@ -1,27 +1,32 @@
 from math import log2
 
 def strength_meter(password):
+    
     length = len(password)
+    
     possible_symbols = 26
     possible_combos = possible_symbols**length
     bits_of_Entrophy = round(log2(possible_combos),2)
     
     if bits_of_Entrophy <=18.8:
         print("Password is Very Weak")
+
     elif bits_of_Entrophy > 18.8 and bits_of_Entrophy <=37.6:
         print("Password is quite Weak")
+
     elif bits_of_Entrophy >37.6 and bits_of_Entrophy <=56.4:
         print("Password is moderate")
+
     elif bits_of_Entrophy >56.4 and bits_of_Entrophy <=75.2:
         print("Password is Good")
+
     elif bits_of_Entrophy >75.2 and bits_of_Entrophy <=95:
         print("Password is Strong")
+        
     elif bits_of_Entrophy > 95:
         print("Your Password is Very strong") 
 
 
-
-strength_meter("8?k87778888888880909")
 
 
 
